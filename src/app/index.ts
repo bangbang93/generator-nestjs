e2dd5cb2307100494227c2d61e55e016744ad6cd
bangbang93 <bangbang93@163.com>
2024-10-29 16:49:30 +0800

    chore: 给project本身也上eslint

--- src/app/index.ts
+++ src/app/index.ts
@@ -1,35 +1,48 @@
-import {fileURLToPath, URL} from 'url'
-import Generator from 'yeoman-generator'
+import { fileURLToPath } from "url";
+import Generator from "yeoman-generator";
 
 interface IAnswer {
-  appName: string
+  appName: string;
 }
 
 export default class NestjsGenerator extends Generator {
-  private answer!: IAnswer
-  public paths() {
-    this.sourceRoot(fileURLToPath(new URL('../../templates/app', import.meta.url)))
-    this.log(this.templatePath())
+  private answer!: IAnswer;
+  public paths(): void {
+    this.sourceRoot(
+      fileURLToPath(new URL("../../templates/app", import.meta.url)),
+    );
+    this.log(this.templatePath());
   }
 
-  public async prompting() {
+  public async prompting(): Promise<void> {
     this.answer = await this.prompt<IAnswer>([
       {
-        type: 'input',
-        message: 'app name',
-        name: 'appName'
-      }
-    ])
+        type: "input",
+        message: "app name",
+        name: "appName",
+      },
+    ]);
   }
 
-  public async writing() {
-    this.copyTemplate(this.templatePath(), this.destinationPath(), {
-      globOptions: {
-        dot: true,
-        ignore: ['**/node_modules']
-      }
-    }, this.answer)
-    this.fs.copy(this.destinationPath('.env.defaults'), this.destinationPath('.env'))
-    this.fs.move(this.destinationPath('.gitignore-template'), this.destinationPath('.gitignore'))
+  public writing(): void {
+    this.copyTemplate(
+      this.templatePath(),
+      this.destinationPath(),
+      {
+        globOptions: {
+          dot: true,
+          ignore: ["**/node_modules"],
+        },
+      },
+      this.answer,
+    );
+    this.fs.copy(
+      this.destinationPath(".env.defaults"),
+      this.destinationPath(".env"),
+    );
+    this.fs.move(
+      this.destinationPath(".gitignore-template"),
+      this.destinationPath(".gitignore"),
+    );
   }
 }
